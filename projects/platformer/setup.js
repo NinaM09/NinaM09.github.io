de4a74b652714e773100304deaca6ec0654b6392
Nina Martinez <martin114@stcharles.k12.la.us>
2023-09-18 15:00:36 +0000

    platform repush

--- projects/platformer/setup.js
+++ projects/platformer/setup.js
@@ -98,4 +98,5 @@ var collectableList = {
   kennedi: { image: "images/collectables/kennedi-head.png" },
   max: { image: "images/collectables/max-head.png" },
   steve: { image: "images/collectables/steve-head.png" },
+  cherry: { image: "images/collectables/cherryBlossom.png" },
 };
